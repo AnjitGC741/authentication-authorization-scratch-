5ffdf0c0e841afd001118d1e81ac370d4c69dfae
AnzitGC741 <anzit.gc@antsware.com>
2024-05-30 14:18:10 +0545

    first commit

---
+++ authentication-authorization(scratch)/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace authentication_authorization_scratch_.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ authentication-authorization(scratch)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace authentication_authorization_scratch_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
